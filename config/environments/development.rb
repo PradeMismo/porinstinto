4e965b822f0de959183012158329eb6a95bf9ba0
Javier Pradillo <javier.pradillo@nrgconsulting.es>
2014-10-14 17:24:46 +0200

    trying assets to work :(

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,26 +35,7 @@ WebPi::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  # mailer config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.raise_delivery_errors = true
+  # Devise gem default mailer config
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: 587,
-    user_name: ENV['MANDRILL_USERNAME'],
-    password: ENV['MANDRILL_APIKEY']
-  }
-  # config.action_mailer.smtp_settings = {
-  #   address: “smtp.gmail.com”,
-  #   port: 587,
-  #   domain: ENV["GMAIL_DOMAIN"],
-  #   authentication: “plain”,
-  #   enable_starttls_auto: true,
-  #   user_name: ENV["GMAIL_USERNAME"],
-  #   password: ENV["GMAIL_PASSWORD"]
-  # }
 end
