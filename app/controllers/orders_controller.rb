bff9be54103e00f47b4838064eeac403a7f59292
Javier Pradillo <javier.pradillo@nrgconsulting.es>
2015-03-10 17:16:33 +0100

    fix order issues

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,14 +12,14 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(params[:order])
     @shop_items = params[:shop_items] 
-    @order.items = @shop_items.map(&:inspect).join(', ')
+    @order.items = @shop_items.map(&:inspect).join(', ') if @shop_items.present?
     @order.request = request
     if @order.deliver
       flash[:success] = 'Gracias por tu pedido! En breve nos pondremos en contacto contigo para confirmarlo.'
       redirect_to new_order_path
     else
       flash[:error] = 'Error: no se ha podido enviar el pedido.'
-      render :new
+      redirect_to new_order_path
     end
   end
 
