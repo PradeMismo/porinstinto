78287bc5fb5328365a2d80cba227e6d059c7c649
Javier Pradillo González <javier.gonzalez@qvantel.com>
2017-12-20 16:49:36 +0100

    Improve form appearance

--- config/initializers/ranged_datetime_wrapper.rb
+++ config/initializers/ranged_datetime_wrapper.rb
@@ -1,13 +1,13 @@
 SimpleForm.setup do |config|
-  config.wrappers :ranged_datetime, tag: 'div', class: 'form-group col-md-6', error_class: 'has-error' do |b|
+  config.wrappers :ranged_datetime, class: :input, error_class: :field_with_errors do |b|
     b.use :html5
     b.use :placeholder
     b.optional :readonly
 
-    b.use :label, class: 'control-label'
+    ## Inputs
+    b.use :label
     b.use :input, class: 'form-control'
-
-    b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    b.use :hint,  wrap_with: { tag: :span, class: :hint }
+    b.use :error, wrap_with: { tag: :span, class: :error }
   end
 end
