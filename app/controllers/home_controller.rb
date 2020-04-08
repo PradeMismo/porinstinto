1cfa7b58c47f5a9b5870e2709b16b79a96ed0fe1
Javier Pradillo <javier.pradillo@qvantel.com>
2020-04-08 12:42:38 +0200

    fix records in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @slides= Slide.all
     @concerts= Concert.next.first(3)
-    @records= Record.studio.last(3)
+    @records= Record.album.published.first(3)
     #@images= Image.all
     @videos= Video.home
     #@entries= Entry.last(3)
